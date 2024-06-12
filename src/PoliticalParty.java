3229a0b2b46d4d816dba490f0cc6c79392e50070
Nicholas <nicholas.st.parker@gmail.com>
2024-06-11 19:58:52 -0500

    initial commit

---
+++ src/PoliticalParty.java
@@ -0,0 +1,3 @@
+public class PoliticalParty {
+    private String partyName;
+}
--- src/UserInterface.java
+++ src/UserInterface.java
@@ -8,14 +8,48 @@ public class UserInterface {
         QuestionList newQuestionList = new QuestionList();
         Response responseList = new Response();
 
+        int democrat = 0;
+        int republican = 0;
+        int libertarian = 0;
+        int greenParty = 0;
+
         newQuestionList.parseCSVQuestionsAndOptions("questions.csv");
 
         for (Question question : newQuestionList) {
             question.printQuestion();
             int input = Integer.parseInt(scanner.nextLine());
             responseList.add(question.getOption(input));
+
+            switch (input) {
+                case 1:
+                    democrat++;
+                case 2:
+                    republican++;
+                case 3:
+                    libertarian++;
+                case 4:
+                    greenParty++;
+            }
         }
-        System.out.println("----");
-        System.out.println(responseList);
+
+        int max = democrat;
+        String maxName = "You most align with the democratic party.";
+
+        if (republican > max) {
+            max = republican;
+            maxName = "You most align with the republican party.";
+        }
+
+        if (libertarian > max) {
+            max = libertarian;
+            maxName = "You most align with the libertarian party.";
+        }
+
+        if (greenParty > max) {
+            maxName = "You most align with the green party.";
+        }
+
+        System.out.println(maxName);
+
     }
 }
